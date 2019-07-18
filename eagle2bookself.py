dcf994b6b1c370ecf2bb7a6ddf5b89bcdd23267f
Devon <9127315+djmerrill@users.noreply.github.com>
2019-07-18 14:58:49 -0400

    Update eagle2bookself.py

--- eagle2bookself.py
+++ eagle2bookself.py
@@ -1,7 +1,25 @@
 """Eagle2Bookshelf.
 
+This program converts EAGLE board files (.brd) to bookshelf format files.
+Specifically this program outputs block component files (.blocks), netlist files (.nets), and net weight files (.wts).
+These files are sutible for academic IC placement programs.
 
-##############################################################################
+This version DOES account for pin placement. But, this feature is unverified.
+This version does not account for net weights. All weights are set to '1'.
+
+This program was written by Devon Merrill (devon@ucsd.edu).
+
+Usage:
+  eagle2bookshelf.py -h | --help
+  eagle2bookshelf.py --brd <BRD> --output_prfx <STEM_NAME> --userid <USERID>
+
+-h --help                      Show this message.
+-i --brd BRD                   The EAGLE .brd file to convert.
+-o --output_prfx STEM_NAME     The stem name for the new files. Includes directory.
+--userid USERID                Your name and contact.
+"""
+
+LICENCE = """
 BSD 3-Clause License
 
 Copyright (c) 2015-2018, The Regents of the University of California
@@ -31,26 +49,6 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-##############################################################################
-
-This program converts EAGLE board files (.brd) to bookshelf format files.
-Specifically this program outputs block component files (.blocks), netlist files (.nets), and net weight files (.wts).
-These files are sutible for academic IC placement programs.
-
-This version DOES account for pin placement. But, this feature is unverified.
-This version does not account for net weights. All weights are set to '1'.
-
-This program was written by Devon Merrill (devon@ucsd.edu).
-
-Usage:
-  eagle2bookshelf.py -h | --help
-  eagle2bookshelf.py --brd <BRD> --output_prfx <STEM_NAME> --userid <USERID>
-
--h --help                      Show this message.
--i --brd BRD                   The EAGLE .brd file to convert.
--o --output_prfx STEM_NAME     The stem name for the new files. Includes directory.
---userid USERID                Your name and contact.
 """
 
 import datetime
